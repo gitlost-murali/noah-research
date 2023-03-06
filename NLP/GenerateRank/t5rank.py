e0fbb2773d1a0932da7fffb38f9d9e7464472bab
gitlost-murali <kmanoharmurali@gmail.com>
2023-03-06 21:36:49 +0100

    fix: pad token in start instead of eos

--- NLP/GenerateRank/t5rank.py
+++ NLP/GenerateRank/t5rank.py
@@ -131,7 +131,7 @@ class TextDataset(Dataset):
                 batch_encoding["decoder_input_ids"] = shift_tokens_right(
                     batch_encoding["labels"], pad_token_id=self.tokenizer.pad_token_id
                 )
-                # replace first token with pad token instead of eos as per T5 docs
+                # fix: replace first token with pad token instead of eos as per T5 docs
                 batch_encoding["decoder_input_ids"][0][0] = self.tokenizer.pad_token_id
                 for k, v in batch_encoding.items():
                     batch_encoding[k] = v.squeeze(0)
@@ -152,6 +152,7 @@ class TextDataset(Dataset):
                 batch_encoding["decoder_input_ids"] = shift_tokens_right(
                     batch_encoding["labels"], pad_token_id=self.tokenizer.pad_token_id
                 )
+                # fix: replace first token with pad token instead of eos as per T5 docs
                 batch_encoding["decoder_input_ids"][0][0] = self.tokenizer.pad_token_id
                 for k, v in batch_encoding.items():
                     batch_encoding[k] = v.squeeze(0)

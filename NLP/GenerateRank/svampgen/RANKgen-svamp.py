2950576b8a1039c4472a46a35647341b62a05dde
gitlost-murali <kmanoharmurali@gmail.com>
2023-04-19 20:03:18 +0200

    add rankgen natural lang and number replacement v2

--- NLP/GenerateRank/svampgen/RANKgen-svamp.py
+++ NLP/GenerateRank/svampgen/RANKgen-svamp.py
@@ -6,6 +6,8 @@
 # In[1]:
 
 
+import re
+import random
 import json
 with open("./t5_preds.json", "r") as fh:
     samples_ans = json.load(fh)
@@ -108,14 +110,53 @@ from rich.progress import track
 
 
 # In[ ]:
+def replace_numholders(mwp, equation):
+    """
+    Replace the number placeholders in the equation with the actual numbers
+    Example: number0 * (number1 + number0) -> 2 * (3 + 2)
+    """
+    # count the number of placeholders using regex
+    num_placeholders = (re.findall(r"number\d+", equation))
+    # only consider unique numbers
+    num_placeholders = list(set(num_placeholders))
+
+    number_mapper = dict()
+    for i in range(len(num_placeholders)):
+        num = random.randint(1, 50)
+        equation = equation.replace(f"number{i}", str(num))
+        mwp = mwp.replace(f"number{i}", str(num))
+        number_mapper[f"number{i}"] = str(num)
+    
+    return mwp, equation, number_mapper
+
+def replace_numholds_eqns(equations, number_mapper):
+    result_equations = []
+    for eq in equations:
+        for k,v in number_mapper.items():
+            eq = eq.replace(k,v)
+        
+        result_equations.append(eq)
+    
+    return result_equations
 
 
 math_eqn_acc = 0
 natural_eqn_acc = 0
+number_eqn_acc = 0
+naturalnumber_eqn_acc = 0
 for item in track(samples_ans, total = len(samples_ans)):
+    numberitem = dict()
+    numberitem['prob'], numberitem['correct_answer'], numbermapper = replace_numholders(item['prob'], item['correct_answer'])
+    numberitem['equations'] = replace_numholds_eqns(item['equations'], numbermapper)
+
     prefix_vectors = rankgen_encoder.encode([item["prob"]+" Give the answer in equation form"], vectors_type="prefix")
     suffix_vectors = rankgen_encoder.encode(item["equations"], vectors_type="suffix")
     natural_suffix_vectors = rankgen_encoder.encode([equation_to_natural_language(it) for it in item["equations"]], vectors_type="suffix")
+
+    num_prefix_vectors = rankgen_encoder.encode([numberitem["prob"]+" Give the answer in equation form"], vectors_type="prefix")
+    num_suffix_vectors = rankgen_encoder.encode(numberitem["equations"], vectors_type="suffix")
+    num_natural_suffix_vectors = rankgen_encoder.encode([equation_to_natural_language(it) for it in numberitem["equations"]], vectors_type="suffix")
+
 
     scores = torch.matmul(prefix_vectors["embeddings"], suffix_vectors["embeddings"].transpose(1,0))
     best_eqnidx_rankgen = torch.argmax(scores)
@@ -123,8 +164,16 @@ for item in track(samples_ans, total = len(samples_ans)):
 
     natural_scores = torch.matmul(prefix_vectors["embeddings"], natural_suffix_vectors["embeddings"].transpose(1,0))
     best_eqnidx_rankgen_nat = torch.argmax(natural_scores)
-    if item["gt"][best_eqnidx_rankgen_nat] == 1: natural_eqn_acc+=1
-    
+    if item["gt"][best_eqnidx_rankgen_nat] == 1: natural_eqn_acc+=1    
+
+    number_scores = torch.matmul(num_prefix_vectors["embeddings"], num_suffix_vectors["embeddings"].transpose(1,0))
+    num_best_eqnidx_rankgen = torch.argmax(number_scores)
+    if item["gt"][num_best_eqnidx_rankgen] == 1:  number_eqn_acc+=1
+
+    number_natural_scores = torch.matmul(num_prefix_vectors["embeddings"], num_natural_suffix_vectors["embeddings"].transpose(1,0))
+    numnat_best_eqnidx_rankgen_nat = torch.argmax(number_natural_scores)
+    if item["gt"][numnat_best_eqnidx_rankgen_nat] == 1: naturalnumber_eqn_acc+=1    
+
 
 # In[ ]:
 

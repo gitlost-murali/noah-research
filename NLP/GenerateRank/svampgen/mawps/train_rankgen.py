70b38cf9006ec55352c9a57a947244959890b4ad
Murali Manohar Chowdary Kondragunta M.M.C.Kondragunta@student.rug.nl <s5397294@login1.internal.hpc.rug.nl>
2023-05-29 20:29:25 +0200

    mawps rankgen code

---
+++ NLP/GenerateRank/svampgen/mawps/train_rankgen.py
@@ -0,0 +1,161 @@
+# import autotokenizer
+import json
+import torch
+import argparse
+from tqdm import tqdm
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from transformers import T5Tokenizer, AutoModel
+
+class MathWordProblemDataset(Dataset):
+    def __init__(self, problems, tokenizer, device):
+        self.problems = problems
+        self.tokenizer = tokenizer
+        self.device = device
+
+    def __len__(self):
+        return len(self.problems)
+
+    def __getitem__(self, idx):
+        problem = self.problems[idx]
+        mwp = problem['prob']
+        equations = problem['equations']
+        local_gt = problem['gt']
+
+        # Add prefixes for MWP and equations
+        mwp = 'pre ' + mwp
+        equations = ['suffi ' + equation for equation in equations]
+
+        mwp_encoded = self.tokenizer(mwp, return_tensors="pt", padding="max_length", max_length=200).to(self.device)
+        equations_encoded = [self.tokenizer(equation, return_tensors="pt", padding="max_length", max_length=64).to(self.device) for equation in equations]
+        return mwp_encoded, equations_encoded, torch.tensor(local_gt, dtype=torch.float)
+
+def contrastive_loss(problems_embeddings, equations_embeddings, gt, temperature=1.0):
+    problems_embeddings = problems_embeddings.unsqueeze(1)
+    scores = torch.matmul(problems_embeddings, equations_embeddings.transpose(1, 2)) / temperature
+    scores = scores.squeeze(1)
+    logits = torch.sigmoid(scores)
+    max_indices = logits.argmax(dim=1)
+    preds = gt[torch.arange(len(gt)), max_indices]
+    # calculate the number of 1's in x
+    preds_corrected = (preds == 1).sum()
+    # Compute the loss using binary cross-entropy
+    loss = torch.nn.BCELoss()(logits, gt)        
+    return loss, preds_corrected
+
+def load_json(path):
+    with open(path) as f:
+        return json.load(f)
+
+def squeeze_batch(batch):
+    for k, v in batch.items():
+        batch[k] = v.squeeze(1)
+    return batch
+
+def main():
+    # create python arg parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str, default="kalpeshk2011/rankgen-t5-xl-all")
+    parser.add_argument("--model_size", type=str, default=None)
+    parser.add_argument("--device", type=str, default='cuda' if torch.cuda.is_available() else 'cpu')
+    parser.add_argument("--trainfile", type=str, default="traint5_preds.json")
+    parser.add_argument("--valfile", type=str, default="t5_preds.json")
+
+    parser.add_argument("--batch_size", type=int, default=2)
+    parser.add_argument("--lr", type=float, default=5e-6)
+    parser.add_argument("--epochs", type=int, default=10)
+    args = parser.parse_args()
+
+    model_path = args.model_path
+    model_size = args.model_size
+    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = torch.device(args.device)
+    trainfile = args.trainfile
+    valfile = args.valfile
+    batch_size = args.batch_size
+    num_epochs = args.epochs
+
+    assert model_path in ["kalpeshk2011/rankgen-t5-xl-all", "kalpeshk2011/rankgen-t5-xl-pg19", "kalpeshk2011/rankgen-t5-base-all", "kalpeshk2011/rankgen-t5-large-all"]
+    if model_size is None:
+        if "t5-large" in model_path or "t5_large" in model_path:
+            model_size = "large"
+        elif "t5-xl" in model_path or "t5_xl" in model_path:
+            model_size = "xl"
+        else:
+            model_size = "base"
+    else:
+        model_size = model_size
+
+    model = AutoModel.from_pretrained(model_path, trust_remote_code=True)
+    model.to(device)
+    model.train()
+    tokenizer = T5Tokenizer.from_pretrained(f"google/t5-v1_1-{model_size}")
+
+    train_problems = load_json(trainfile)  # Your training data
+    val_problems = load_json(valfile)  # Your validation data
+
+    train_dataset = MathWordProblemDataset(train_problems, tokenizer, device)
+    val_dataset = MathWordProblemDataset(val_problems, tokenizer, device)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
+
+
+    optimizer = optim.AdamW(model.parameters(), lr=args.lr)
+
+    # Training loop
+    for epoch in range(num_epochs):
+        model.train()
+        total_loss = 0.0
+        step = 0
+        train_acc = 0
+        for mwp_input, equations_input, gt in tqdm(train_loader):
+            step += 1
+            optimizer.zero_grad()
+
+            # Encode the MWP
+            mwp_input = squeeze_batch(mwp_input)
+            mwp_encoded = model(**mwp_input)
+            # Encode the equations
+            equations_input = [squeeze_batch(equation) for equation in equations_input]
+            equation_embeddings = [model(**equation) for equation in equations_input]
+            equations_encoded = torch.stack(equation_embeddings, dim=1)
+
+            # Compute the contrastive loss
+            loss, bz_num_corrected = contrastive_loss(mwp_encoded, equations_encoded, gt.to(device))
+            train_acc += bz_num_corrected
+
+            # Backpropagate and update the weights
+            loss.backward()
+            optimizer.step()
+
+            if step % 100 == 0:
+                print(f"Epoch {epoch+1}, Step {step}, Loss: {loss.item()}")
+
+            total_loss += loss.item()
+
+        print(f"Epoch {epoch+1}, Loss: {total_loss / len(train_loader)}, acc: {train_acc / len(train_dataset)}")
+
+        # Evaluate the model on the validation set
+        model.eval()
+        total_val_loss = 0.0
+        val_acc = 0
+        with torch.inference_mode():
+            for mwp_input, equations_input, gt in val_loader:
+                mwp_input = squeeze_batch(mwp_input)
+                mwp_encoded = model(**mwp_input)
+                equations_input = [squeeze_batch(equation) for equation in equations_input]
+                equation_embeddings = [model(**equation) for equation in equations_input]
+                equations_encoded = torch.stack(equation_embeddings, dim=1)
+
+                # Compute the contrastive loss for the validation set
+                val_loss, bzval_num_corrected = contrastive_loss(mwp_encoded, equations_encoded, gt.to(device))
+                val_acc += bzval_num_corrected
+
+                total_val_loss += val_loss.item()
+
+        print(f"Validation Epoch {epoch+1}, Loss: {total_val_loss / len(val_loader)}, acc: {val_acc / len(val_dataset)}")
+
+if __name__ == "__main__":
+    main()
